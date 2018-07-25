253421a69a38a7eb7bd0c9e69cc7ea32f6a7161f
CXQ <714867833@qq.com>
2018-07-25 23:03:02 +0800

    second commit by CXQ

---
+++ public/js/global.js
@@ -0,0 +1,7 @@
+$("ul.layui-nav.layui-layout-left").on("click","li.layui-nav-item>a",function(event){
+    // event.preventDefault();
+    //alert("ok");
+});
+layui.use('element', function () {
+    var element = layui.element;
+});
